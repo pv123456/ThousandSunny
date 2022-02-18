bf0c69706d4864c0eb08388b79c37b6ae9afd1d5
Pinesh Vala <P2588742@my365.dmu.ac.uk>
2022-02-18 20:09:42 +0000

    I have created 6 test to for my attributes

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public bool SupplierDiscountPrice { get; set; }
+        public DateTime StartDateSupplier { get; set; }
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierEmail { get; set; }
+        public string SupplierAddress { get; set; }
     }
 }
