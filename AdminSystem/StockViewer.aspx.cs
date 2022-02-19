f77f41af3e4204e4fb445694c78636b27baacb15
Sabrina Harris <P2624536@my365.dmu.ac.uk>
2022-02-19 19:24:13 +0000

    Started week 21

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsStock AnStock = new clsStock();
+        AnStock = (clsStock)Session["AnStock"];
+        Response.Write(AnStock.StockID);
     }
 }
