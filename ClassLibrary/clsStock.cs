e896a4545cea01990c68f92498ec05b2e42b9113
Sabrina Harris <P2624536@my365.dmu.ac.uk>
2022-03-10 21:40:04 +0000

    i have just re done all my work, it now all seems to be working.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,7 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
-        public string StockID { get; set; }
+        public bool StockAvailability { get; set; }
+        public DateTime StockLastAdded { get; set; }
+        public int StockID { get; set; }
+        public decimal StockPrice { get; set; }
+        public object StockName { get; set; }
+        public object StockDescription { get; set; }
     }
 }
