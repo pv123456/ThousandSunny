a48a4595031ff2e66f37f3502fd00ca64eb9f652
Sabrina Harris <P2624536@my365.dmu.ac.uk>
2022-03-15 20:38:18 +0000

    completed week 23

--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -95,5 +95,148 @@ namespace Testing3
             Assert.AreEqual(AnStock.StockDescription, TestData);
         }
 
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create 
+            clsStock AnStock = new clsStock();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //create some test data to use with the method 
+            Int32 StockID = 11;
+            //invoke the method 
+            Found = AnStock.Find(StockID);
+            //test to see that the result is correct 
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestStockIDFound()
+        {
+            //create an instance of the class we want to create 
+            clsStock AnStock = new clsStock();
+            //boolean variable to store the result of the search 
+            Boolean Found = false;
+            //boolean  variable to record if data is OK 
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StockID = 11;
+            //invoke the method 
+            Found = AnStock.Find(StockID);
+            //check the stock id
+            if (AnStock.StockID != 11)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct 
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestStockpriceFound()
+        {
+            //make an instance of Stock
+            clsStock AnStock = new clsStock();
+            //boolean variable 
+            Boolean Found = false;
+            Boolean OK = true;
+            //test data
+            Int32 StockID = 11;
+            //invoke find method
+            Found = AnStock.Find(StockID);
+            //check price  of stock matches
+            if (AnStock.StockPrice != 2)
+            {
+                OK = false;
+            }
+            //check if result matches
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStockLastAddedFound()
+        {
+            //make an instance of Stock
+            clsStock AnStock = new clsStock();
+            //boolean variable 
+            Boolean Found = false;
+            Boolean OK = true;
+            //test data
+            Int32 StockID = 11;
+            //invoke find method
+            Found = AnStock.Find(StockID);
+            //check last added of stock matches
+            if (AnStock.StockLastAdded != Convert.ToDateTime("16/09/2015"))
+            {
+                OK = false;
+            }
+            //check if result matches
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStockNameFound()
+        {
+            //make an instance of Stock
+            clsStock AnStock = new clsStock();
+            //boolean variable 
+            Boolean Found = false;
+            Boolean OK = true;
+            //test data
+            Int32 StockID = 11;
+            //invoke find method
+            Found = AnStock.Find(StockID);
+            //check name of stock matches
+            if (AnStock.StockName != Convert.ToString("Test Name"))
+            {
+                OK = false;
+            }
+            //check if result matches
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStockAvailabilityFound()
+        {
+            //make an instance of Stock
+            clsStock AnStock = new clsStock();
+            //boolean variable 
+            Boolean Found = false;
+            Boolean OK = true;
+            //test data
+            Int32 StockID = 11;
+            //invoke find method
+            Found = AnStock.Find(StockID);
+            //check Avalibility  of stock matches
+            if (AnStock.StockAvailability != true)
+            {
+                OK = false;
+            }
+            //check if result matches
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestStockDescriptionFound()
+        {
+            //make an instance of Stock
+            clsStock AnStock = new clsStock();
+            //boolean variable 
+            Boolean Found = false;
+            Boolean OK = true;
+            //test data
+            Int32 StockID = 11;
+            //invoke find method
+            Found = AnStock.Find(StockID);
+            //check Description  of stock matches
+            if (AnStock.StockDescription != Convert.ToString("Test Description"))
+            {
+                OK = false;
+            }
+            //check if result matches
+            Assert.IsTrue(OK);
+        }
+
     }
 }
