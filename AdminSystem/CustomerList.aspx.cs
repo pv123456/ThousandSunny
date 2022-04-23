33bdaa7bb0982fc926d06686d7ea3cf283d35356
Xavier Faruque <P2607154@my365.dmu.ac.uk>
2022-04-23 20:59:12 +0100

    Lists + collection done

--- AdminSystem/CustomerList.aspx.cs
+++ AdminSystem/CustomerList.aspx.cs
@@ -4,10 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack == false)
+        {
+            DisplayCustomers();
+        }
+
+    }
+
+    void DisplayCustomers()
+    {
+        clsCustomerCollection Customers = new clsCustomerCollection();
+        lstCustomerList.DataSource = Customers.CustomerList;
+        lstCustomerList.DataValueField = "CustID";
+        lstCustomerList.DataTextField = "CustEmail";
+        lstCustomerList.DataBind();
+    }
+
+    protected void lstCustomerList_SelectedIndexChanged(object sender, EventArgs e)
     {
 
     }
---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+
+        public List<clsCustomer> CustomerList 
+        { 
+            get
+            {
+                return mCustomerList;
+            }
+            set
+            {
+                mCustomerList = value;
+            }
+        }
+        public int Count
+        { 
+            get
+            {
+                return mCustomerList.Count;
+            }
+            set
+            {
+
+            }
+        }
+
+
+        public clsCustomer ThisCustomer { get; set; }
+
+        public clsCustomerCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblCustomerManagement_SelectAll");
+            RecordCount = DB.Count;
+            while(Index<RecordCount)
+            {
+                clsCustomer ACustomer = new clsCustomer();
+                ACustomer.CustId = Convert.ToInt32(DB.DataTable.Rows[Index]["CustID"]);
+                ACustomer.CustUsername = Convert.ToString(DB.DataTable.Rows[Index]["CustUsername"]);
+                ACustomer.CustPassword = Convert.ToString(DB.DataTable.Rows[Index]["CustPassword"]);
+                ACustomer.CustEmail = Convert.ToString(DB.DataTable.Rows[Index]["CustEmail"]);
+                ACustomer.CustDOB = Convert.ToDateTime(DB.DataTable.Rows[Index]["CustDOB"]);
+                ACustomer.Over18 = Convert.ToBoolean(DB.DataTable.Rows[Index]["Over18"]);
+                mCustomerList.Add(ACustomer);
+                Index++;
+            }
+
+        }
+    }
+}
